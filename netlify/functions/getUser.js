af626eb6ad3b2b0ca49202527e49cbbdd930f61d
Daniel Scholtus <dscholtus@paychex.com>
2022-10-22 22:06:24 -0300

    Add mocks and update getuser to use mock

--- netlify/functions/getUser.js
+++ netlify/functions/getUser.js
@@ -1,25 +1,4 @@
-const mockUser = {
-    id: 'acf7a6da-ec3a-4e0d-adbc-394cf49ccd91',
-    name: 'Flavio',
-    lastname: 'Tosi',
-    version: 1,
-    lessons: [
-        {
-            id: '30be14b5-99aa-46cb-a009-d5456a5d5897',
-            completed: true,
-        },
-    ],
-    questions: [
-        {
-            id: '76e15ec2-a28a-4565-a81c-87aea53ab835',
-            answer: '27952e6b-7ac9-4cba-bcc6-730745be5dd5',
-        },
-        {
-            id: 'b0c62b4d-29cf-4b3b-b863-01d01f2972aa',
-            answer: 'El Espíritu Santo',
-        },
-    ],
-}
+import mockUser from "../../mocks/mockUser"
 
 const handler = async (event, context) => {
 
